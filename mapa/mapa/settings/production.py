aaae2801f6debf15139733d4ac07263f3042746a
Gonçalo Silva <goncalo.silva@elastictek.com>
2018-10-15 11:46:31 +0100

    fg

--- mapa/mapa/settings/production.py
+++ mapa/mapa/settings/production.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'mapa.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(os.path.join(BASE_DIR, 'db.sqlite3')),
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'mapa',
-        'USER': 'admin',
-        'PASSWORD': 'Admin123',
-        'HOST': '192.168.0.118',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.join(BASE_DIR, 'db.sqlite3')),
+        #'ENGINE':'django.db.backends.mysql',
+        #'NAME': 'mapa',
+        #'USER': 'admin',
+        #'PASSWORD': 'Admin123',
+        #'HOST': '192.168.0.118',
+        #'PORT': ''
     }
 }
 
